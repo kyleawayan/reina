d8215dce5b879f622e5078e31f7e3c3e7eb38a68
kyleawayan <56655579+kyleawayan@users.noreply.github.com>
2020-10-14 16:44:44 -0700

    add dynamic commands

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const i18n = require("i18n");
 const locale = require(`./locales/en.json`);
 const StringUtil = require("./util/stringUtil");
 
+const fs = require("fs");
+client.commands = new Discord.Collection();
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
 i18n.configure({
   locales: ["en"],
   directory: `${__dirname}/locales`,
@@ -16,6 +26,7 @@ client.on("ready", () => {
 });
 
 client.on("message", (msg) => {
+  // hey function used from https://github.com/ReFruity/EzBot/blob/master/util/stringUtil.js
   function hey(transcription, words) {
     return words.includes(transcription) > 0;
   }
@@ -25,17 +36,69 @@ client.on("message", (msg) => {
   }
 });
 
-client.on("message", (msg) => {
+client.on("message", (message) => {
+  const prefix = "reina";
   // "reina" will be the "prexix"
-  if (StringUtil.isTranscriptionContains(msg.content, locale.reina)) {
-    // make a better way to handle commands: ex. if the message is "reina play", then take the second word (play)
-    // and look for the existing js file in /things/
-    // so since it's play, it would execute stuff in play.js
-    var command = [];
-    command = msg.content.split(" ")[1];
-    console.log(command);
+  // make a better way to handle commands: ex. if the message is "reina play", then take the second word (play)
+  // and look for the existing js file in /things/
+  // so since it's play, it would execute stuff in play.js
 
-    msg.channel.send(command + "command");
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (!client.commands.has(command)) return;
+
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
+  }
+});
+
+client.on("message", (message) => {
+  const prefix = "rei";
+  // "reina" will be the "prexix"
+  // make a better way to handle commands: ex. if the message is "reina play", then take the second word (play)
+  // and look for the existing js file in /things/
+  // so since it's play, it would execute stuff in play.js
+
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (!client.commands.has(command)) return;
+
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
+  }
+});
+
+client.on("message", (message) => {
+  const prefix = "rey";
+  // "reina" will be the "prexix"
+  // make a better way to handle commands: ex. if the message is "reina play", then take the second word (play)
+  // and look for the existing js file in /things/
+  // so since it's play, it would execute stuff in play.js
+
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  if (!client.commands.has(command)) return;
+
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
   }
 });
 
--- util/stringUtil.js
+++
@@ -1,9 +0,0 @@
-// used from https://github.com/ReFruity/EzBot/blob/master/util/stringUtil.js
-
-module.exports = {
-  isTranscriptionContains: (transcription, words) => {
-    var firstWord = [];
-    firstWord = transcription.split(" ")[0];
-    return words.includes(firstWord) > 0;
-  },
-};
