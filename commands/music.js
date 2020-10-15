8442384464acce480bd5227cece0526d4e62b350
kyleawayan <56655579+kyleawayan@users.noreply.github.com>
2020-10-14 17:58:22 -0700

    play mp3 file

--- commands/music.js
+++ commands/music.js
@@ -1,12 +1,15 @@
-const yts = require("yt-search");
-
 module.exports = {
   // use https://github.com/przemyslawpluta/node-youtube-dl
   // pass url to music.js and make temporary audio file, (make some queue system too), and use discord.js to play it
   name: "play",
-  description: "play!",
+  description: "play music",
   execute(message, args) {
-    console.log(args);
-    message.channel.send("Pong.");
+    async function music() {
+      if (message.member.voice.channel) {
+        const connection = await message.member.voice.channel.join();
+        connection.play("./selene.mp3");
+      }
+    }
+    music();
   },
 };
