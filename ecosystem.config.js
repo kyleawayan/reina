3bb4288e4f3476b808b9f7f60ebdd92db92d1cb7
kyleawayan <56655579+kyleawayan@users.noreply.github.com>
2020-10-15 02:47:40 -0700

    nevermind

---
+++ ecosystem.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  apps: [
+    {
+      script: "index.js",
+      watch: ".",
+    },
+    {
+      script: "./service-worker/",
+      watch: ["./service-worker"],
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "reina",
+      ref: "origin/main",
+      repo: "https://github.com/kyleawayan/reina",
+      path: "/home/reina",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
+};
