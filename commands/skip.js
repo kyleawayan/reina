0eb8afdbcfcf5ac4d298a7852677fe4a72618fb2
kyleawayan <56655579+kyleawayan@users.noreply.github.com>
2020-10-15 22:18:53 -0700

    restore back to old code / restore skip command

--- commands/skip.js
+++ commands/skip.js
@@ -3,5 +3,6 @@ module.exports = {
   description: "skip the currently playing song",
   execute(message) {
     const serverQueue = message.client.queue.get(message.guild.id);
+    serverQueue.connection.dispatcher.end();
   },
 };
