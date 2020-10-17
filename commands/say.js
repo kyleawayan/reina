34b2753cae71260df481eaf6b60b210c1174892f
kyleawayan <56655579+kyleawayan@users.noreply.github.com>
2020-10-17 01:44:16 -0700

    delete extra commands

--- commands/say.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "say",
-  description: "say something through reina",
-  usage: "kyle is stupid",
-  execute(message, args) {
-    message.channel.send(args.slice(0, 999).join(" "));
-    message.delete();
-  },
-};
