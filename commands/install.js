87daa6bf2b62ec74d0cb0bca8dafefb389b7616f
kyleawayan <56655579+kyleawayan@users.noreply.github.com>
2020-10-16 22:43:07 -0700

    complete install/uninstall script

--- commands/install.js
+++ commands/install.js
@@ -21,7 +21,7 @@ module.exports = {
     if (message.author.id == process.env.id) {
       message.channel.send(quiz.question).then(() => {
         message.channel
-          .awaitMessages(filter, { max: 1, time: 30000, errors: ["time"] })
+          .awaitMessages(filter, { max: 1, time: 10000, errors: ["time"] })
           .then((collected) => {
             // agreed
             message.channel.send(`cloning repo...`);
@@ -36,65 +36,74 @@ module.exports = {
 
                 const init = require("../temp/init.json");
 
-                message.channel.send(
-                  "installing " + init.dependecies.join(", ") + "..."
-                );
-                init.dependecies.unshift("install");
-                const deps = spawn("npm", init.dependecies);
-                deps.stdout.on("data", function (data) {
-                  console.log(data.toString());
-                });
-                deps.stderr.on("data", function (data) {
-                  console.log(data.toString());
-                });
+                  message.channel.send(
+                    "installing " + init.dependecies.join(", ") + "..."
+                  );
+                  init.dependecies.unshift("install");
+                  const deps = spawn("npm", init.dependecies);
+                  deps.stdout.on("data", function (data) {
+                    console.log(data.toString());
+                  });
+                  deps.stderr.on("data", function (data) {
+                    console.log(data.toString());
+                  });
+                  deps.on("exit", function() {
+                    delete require.cache[require.resolve(`./${init.name}.js`)];
+                    try {
+                      const newCommand = require(`./${init.name}.js`);
+                      message.client.commands.set(newCommand.name, newCommand);
+                    } catch (error) {
+                      console.error(error);
+                      message.channel.send(
+                        `there was an error while reloading the command ${init.name}:\n\`${error.message}\``
+                      );
+                    }
+                    message.channel.send(`**installed ${init.name}!**`);
 
-                message.channel.send(
-                  `moving ${init.name} to commands directory...`
-                );
-                const temp = path.join(__dirname, "..", "temp", init.filename);
-                const dest = path.join(
-                  __dirname,
-                  "..",
-                  "commands",
-                  `${init.name}.js`
-                );
-
-                fs.rename(temp, dest, (err) => {
-                  if (err) {
-                    console.log(err);
-                  }
-                });
-
-                const tempjson = path.join(
-                  __dirname,
-                  "..",
-                  "temp",
-                  "init.json"
-                );
-                const destjson = path.join(
-                  __dirname,
-                  "..",
-                  "commands",
-                  `${init.name}.json`
-                );
-
-                fs.rename(tempjson, destjson, (err) => {
-                  if (err) {
-                    console.log(err);
-                  }
-                });
-
-                message.channel.send(
-                  `installed ${init.name}! please restart reina.`
-                );
-
-                const tempdir = path.join(__dirname, "..", "temp");
-
-                fs.rmdir(tempdir, { recursive: true }, (err) => {
-                  if (err) {
-                    console.log(err);
-                  }
-                });
+                    const tempdir = path.join(__dirname, "..", "temp");
+    
+                    fs.rmdir(tempdir, { recursive: true }, (err) => {
+                      if (err) {
+                        console.log(err);
+                      }
+                    });
+                  })
+  
+                  message.channel.send(
+                    `moving ${init.name} to commands directory...`
+                  );
+                  const temp = path.join(__dirname, "..", "temp", init.filename);
+                  const dest = path.join(
+                    __dirname,
+                    "..",
+                    "commands",
+                    `${init.name}.js`
+                  );
+  
+                  fs.rename(temp, dest, (err) => {
+                    if (err) {
+                      console.log(err);
+                    }
+                  });
+  
+                  const tempjson = path.join(
+                    __dirname,
+                    "..",
+                    "temp",
+                    "init.json"
+                  );
+                  const destjson = path.join(
+                    __dirname,
+                    "..",
+                    "commands",
+                    `${init.name}.json`
+                  );
+  
+                  fs.rename(tempjson, destjson, (err) => {
+                    if (err) {
+                      console.log(err);
+                    }
+                  });
               }
             });
           })
--- commands/uninstall.js
+++ commands/uninstall.js
@@ -9,40 +9,41 @@ module.exports = {
   execute(message, args) {
       if (message.author.id == process.env.id) {
         if (args[0] !== undefined) {
-            const init = require(`../commands/${args[0]}.json`);
-            const commandspath = path.join(__dirname, "..", "commands");
-      
-            message.channel.send(
-              "deleting " + init.name + " from commands directory"
-            );
-            fs.unlink(`${commandspath}/${init.name}.js`, (err) => {
-              if (err) {
-                console.log(err);
-              }
-            });
-      
-            message.channel.send(
-              "uninstalling " + init.dependecies.join(", ") + "..."
-            );
-            init.dependecies.unshift("uninstall");
-            const deps = spawn("npm", init.dependecies);
-            deps.stdout.on("data", function (data) {
-              console.log(data.toString());
-            });
-            deps.stderr.on("data", function (data) {
-              console.log(data.toString());
-            });
-      
-            fs.unlink(`${commandspath}/${init.name}.json`, (err) => {
-              if (err) {
-                console.log(err);
-              }
-            });
-      
-            setTimeout(() => {
-              message.channel.send(`uninstalled ${init.name}! please restart reina.`);
-            }, 1500);
-          } else {
+          const init = require(`../commands/${args[0]}.json`);
+          const commandspath = path.join(__dirname, "..", "commands");
+
+          delete require.cache[require.resolve(`./${init.name}.js`)];
+
+          message.channel.send(
+            "deleting " + init.name + " from commands directory"
+          );
+          fs.unlink(`${commandspath}/${init.name}.js`, (err) => {
+            if (err) {
+              console.log(err);
+            }
+          });
+
+          message.channel.send(
+            "uninstalling " + init.dependecies.join(", ") + "..."
+          );
+          init.dependecies.unshift("uninstall");
+          const deps = spawn("npm", init.dependecies);
+          deps.stdout.on("data", function (data) {
+            console.log(data.toString());
+          });
+          deps.stderr.on("data", function (data) {
+            console.log(data.toString());
+          });
+          deps.on("exit", function  () {
+            message.channel.send(`**uninstalled ${init.name}!**`);
+          });;
+
+          fs.unlink(`${commandspath}/${init.name}.json`, (err) => {
+            if (err) {
+              console.log(err);
+            }
+          });
+        } else {
             message.channel.send("what do you want me to uninstall?");
           }
       }
