b3c3bd6f30c57db2524b844aae33bb4ba9146b9f
kyleawayan <56655579+kyleawayan@users.noreply.github.com>
2020-12-26 22:01:29 -0800

    don't run client.player.play if not in vc

--- commands/play.js
+++ commands/play.js
@@ -4,20 +4,24 @@ module.exports = {
   usage: "niki` or `reina play https://www.youtube.com/watch?v=P_FB84Acp60",
   execute(message, args, client) {
     (async () => {
-      if (
-        !args[0].match(
-          /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g
-        )
-      ) {
-        client.player.play(message, args.slice(0, 999).join(" "), true);
-        if (client.player.isPlaying(message)) {
-          message.react("✅");
+      if (message.member.voice.channel) {
+        if (
+          !args[0].match(
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g
+          )
+        ) {
+          client.player.play(message, args.slice(0, 999).join(" "), true);
+          if (client.player.isPlaying(message)) {
+            message.react("✅");
+          }
+        } else {
+          client.player.play(message, args[0]);
+          if (client.player.isPlaying(message)) {
+            message.react("✅");
+          }
         }
       } else {
-        client.player.play(message, args[0]);
-        if (client.player.isPlaying(message)) {
-          message.react("✅");
-        }
+        message.channel.send(`you need to join a voice channel`);
       }
     })();
   },
